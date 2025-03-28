479e3593509e83f0ec90b2bc0c14c63851c54ad8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-28 20:09:26 -0300

    Resolvido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string cebolinha;
+
+Console.Clear();
+
+Console.WriteLine("Exercico Cebolinha");
+
+Console.WriteLine(@"Qual frase você deseja converter?");
+
+cebolinha = Console.ReadLine();
+cebolinha = cebolinha
+.Replace("r", "l")
+.Replace("R", "L");
+
+Console.WriteLine(cebolinha);
